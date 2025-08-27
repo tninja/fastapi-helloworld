300f7664b81019e45cf9565638a072195fb120be
tninja <tninja@gmail.com>
2025-08-26 20:46:46 -0700

    Add endpoint to greet user by name with path parameter

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 def hello():
     return {"msg": "hello world, Kang's first FastAPI app!"}
+
+# 带路径参数
+@app.get("/hello/{name}")
+def say_hello(name: str):
+    return {"msg": f"Hello, {name}!"}
